eb31648377cb6c0d5f42f4c52d4cf92d5e772d62
levioawen <kaig93@gmail.com>
2024-03-01 22:07:27 +0300

    add lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include "ArduinoModbus.h"
+#include <ArduinoRS485.h>
+#include <DFRobot_EC.h>
+#include <microDS18B20.h>
 void setup() {
 // write your initialization code here
 }
